663890b32d236af1e13fda2d280d381c6ccb3ff4
ThePurpleOne <jonas@stirnemann.xyz>
2024-07-28 18:51:02 +0200

    Read layout, simpler than regenerating it with algo

--- src/kicad_scripts/pcb_gen/pcb_gen.py
+++ src/kicad_scripts/pcb_gen/pcb_gen.py
@@ -1,7 +1,10 @@
 import pcbnew
-
+from math import sqrt
+import csv
 
 pcb_path = "../test_project/test_project.kicad_pcb"
+ref_layout_path = "../test_project/coils_layout.csv"
+
 
 # Load the PCB file
 pcb = pcbnew.LoadBoard(pcb_path)
@@ -11,31 +14,47 @@ pcb = pcbnew.LoadBoard(pcb_path)
 components = pcb.GetFootprints()
 
 print(f"Found {len(components)} components")
+number_of_coils = len(components)
+nb_row_col = int(sqrt(number_of_coils))
 
 
-# Sort only by number in refereces, not the letters (there can be multiple letters)
-def sort_by_ref(comp):
-    return int(comp.GetReference().strip("ABCDEFGHIJKLMNOPQRSTUVWXYZ"))
+# Read the layout csv file as a 2d array, rows are separated by newlines
+# and columns are separated by commas
+def read_layout_csv(path):
+    layout = []
+    with open(path, "r") as file:
+        reader = csv.reader(file)
+        for row in reader:
+            layout.append(row[:-1])
+    return layout
 
 
-sorted_comp = sorted(components, key=sort_by_ref)
+layout = read_layout_csv(ref_layout_path)
 
-for comp in sorted_comp:
-    print(
-        f"Component {comp.GetReference()} at position ({comp.GetPosition().x}, {comp.GetPosition().y})"
-    )
+
+final_arr = []
+# For each component, find the corresponding position from the ref layout array
+for i in range(nb_row_col):
+    row = []
+    for j in range(nb_row_col):
+        for component in components:
+            if component.GetReference() == layout[i][j]:
+                row.append(component)
+                break
+    final_arr.append(row)
+
+
+## Print the sorted components as a 2D array
+for i in range(nb_row_col):
+    for j in range(nb_row_col):
+        print(f"{final_arr[i][j].GetReference():^10} ", end="")
+    print("")
+
 
 # Move the first component to a new position
 new_position = pcbnew.wxPointMM(float(200), float(200))
 
-sorted_comp[0].SetX(new_position.x)
-sorted_comp[0].SetY(new_position.y)
-
-print(
-    f"Component {sorted_comp[0].GetReference()} moved to position ({new_position.x}, {new_position.y})"
-)
-
 # Save the updated PCB file
-pcb.Save(pcb_path)
+# pcb.Save(pcb_path)
 
 print("PCB file updated")
